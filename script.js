0beca1a464f543589548308e32fd9b4d5561a61f
Fahim-Hassan-Ovi <fahim.hassan.ovi@gmail.com>
2024-12-20 20:30:20 +0600

    added the dyanmic mulitple button click section of comment and view

--- script.js
+++ script.js
@@ -5,19 +5,19 @@ const loadAllPosts = async (category) => {
     // way-2
     // console.log(`https://openapi.programming-hero.com/api/retro-forum/posts${category?`?category=${category}`:''}`);
 
-    const response = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts${category? `?category=${category}` : ''}`)
+    const response = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts${category ? `?category=${category}` : ''}`)
     const data = await response.json()
     displayAllPost(data.posts);
 }
 
-const displayAllPost = (posts) =>{
+const displayAllPost = (posts) => {
     const postContainer = document.getElementById("post-container");
-    posts.forEach((post) =>{
+    posts.forEach((post) => {
         const div = document.createElement('div');
-        div.innerHTML= `
+        div.innerHTML = `
         <div class="p-6 lg:p-12 flex gap-6 lg:flex-row flex-col items-center lg:items-start bg-[#F3F3F5] rounded-3xl">
             <div class="indicator">
-              <span class="indicator-item badge ${post.isActive ? " bg-green-600" : "bg-red-500" }"></span>
+              <span class="indicator-item badge ${post.isActive ? " bg-green-600" : "bg-red-500"}"></span>
               <div class="avatar">
                 <div class="w-24 rounded-xl">
                   <img src=${post.image} alt="">
@@ -53,7 +53,7 @@ const displayAllPost = (posts) =>{
                   </div>
                 </div>
                 <div class="opacity-100">
-                  <button id="addToList" onclick="markAsRead()" data-post='${JSON.stringify(post)}'
+                  <button id="addToList" onclick="markAsRead('${post.description}','${post.view_count}')" data-post='${JSON.stringify(post)}' 
                     class="addToList btn btn-circle bg-green-500 btn-sm">
                     <i class="fa-solid fa-envelope-open text-white"></i>
                   </button>
@@ -64,6 +64,25 @@ const displayAllPost = (posts) =>{
         `
         postContainer.appendChild(div);
     })
+}
+
+const markAsRead = (description, view_count) => {
+
+    const markAsReadContainer = document.getElementById('markAsReadContainer');
+    const div = document.createElement("div");
+    div.innerHTML = `
+    <div class="flex justify-between p-2 lg:p-3 bg-white rounded-2xl items-center gap-3">
+                <div class="lg:w-4/5 w-11/12 ">
+                  <p>
+                    ${description}
+                  </p>
+                </div>
+                <div class="lg:w-1/5 w-4/12 flex justify-end">
+                  <p><i class="fa-regular fa-eye"></i> ${view_count}</p>
+                </div>
+               </div>
+`
+markAsReadContainer.appendChild(div);
 }
 
 loadAllPosts();
